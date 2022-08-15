36c39cd115c833b3e1494e3da748f413bcf6324c
sato <ysudqfs@163.com>
2022-08-15 14:54:34 +0800

    add struct to map

--- utils/mail/mail_test.go
+++ utils/mail/mail_test.go
@@ -1,6 +1,7 @@
 package mail
 
 import (
+	"blog-backend/utils/struct_util"
 	"fmt"
 	"os"
 	"sync"
@@ -19,19 +20,22 @@ func TestSendEmail(t *testing.T) {
 		"url":            "https://www.baidu.com",
 	}
 	fmt.Println(tplComment)
-
-	tpl2 := map[string]string{
-		"username": "狗蛋",
-		"site":     "site",
-		"time":     "2022.08.15",
-		"title":    "哔哩哔哩源码分析",
-		"summary":  "哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析",
-		"url":      "https://www.baidu.com",
+	newUpdate := NewUpdate{
+		Username: "狗蛋",
+		Site:     "site",
+		Time:     "2022.08.15",
+		Title:    "哔哩哔哩源码分析",
+		Summary:  "哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析哔哩哔哩源码分析",
+		Url:      "https://www.baidu.com",
 	}
-	body := os.Expand(NewUpdateTpl, func(k string) string { return tpl2[k] })
-	for i := 0; i <= 1; i++ {
+	bodyMap, err := struct_util.StructToMap(newUpdate, "json", "StructToMap")
+	if err != nil {
+		return
+	}
+	body := os.Expand(NewUpdateTpl, func(k string) string { return bodyMap[k].(string) })
+	for i := 0; i < 1; i++ {
 		wg.Add(1)
-		go SendEmail([]string{"ysudqfs@163.com"}, "123344", body, wg)
+		go SendEmail([]string{"ysudqfs@163.com"}, newUpdate.Title, body, wg)
 	}
 	wg.Wait()
 }
